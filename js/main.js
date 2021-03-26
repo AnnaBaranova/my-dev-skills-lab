e5471e0cc99a6a7431ad3807d37d6072983b6d0b
Anna Baranova <asbaranova1@gmail.com>
2021-03-25 20:49:41 -0400

    add functions remove and eventlistener keypress

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 
 
 $("#button-addon2").on("click", addSkill);
+$("#input").keypress(function (e){
+  if (e.keyCode ===13){
+    addSkill();
+  }
+});
 
 
-
-function addSkill(){
-    let $skill = $("#input").val();
-    // console.log ($skill);
-    const $newItem = $(`<li class="list-group-item">
-      <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." checked> ${$skill}
-    </li>`);
+function addSkill() {
+  let $skill = $("#input").val();
+  // console.log ($skill);
+  if ($skill) {
+    const $newItem = $(`<div class="row dev-skills"><button type="button" class="close" style="background-color:#dc3545" aria-label="Close">
+    <span aria-hidden="false">&times;</span>
+  </button><li class="list" style="margin-left:5px">${$skill}</li></div>`);
     $(".list-group").append($newItem);
     $("#input").val(null);
+  }
+
+$("div.dev-skills").on("click", function (){
+  const removeBtn = $(this);
+  console.log (this)
+  removeBtn.remove();
+ });
 };
+
+
+
+
